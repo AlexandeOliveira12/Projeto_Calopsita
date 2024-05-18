9698b756844ab31657dc1c5efb5c7f4797bce958
AlexandeOliveira12 <lebella321@gmail.com>
2024-05-18 19:51:47 -0300

    first commit

---
+++ src/Calopsita.java
@@ -0,0 +1,29 @@
+public class Calopsita {
+  private String cor;
+  private String especie;
+  private String tamanho;
+
+  public String getCor() {
+    return cor;
+  }
+
+  public void setCor(String cor) {
+    this.cor = cor;
+  }
+
+  public String getEspecie() {
+    return especie;
+  }
+
+  public void setEspecie(String especie) {
+    this.especie = especie;
+  }
+
+  public String getTamanho() {
+    return tamanho;
+  }
+
+  public void setTamanho(String tamanho) {
+    this.tamanho = tamanho;
+  }
+}
---
+++ src/TesteCalopsita.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TesteCalopsita {
+
+  public static void main(String[] args) {
+    Calopsita calopsita01 = new Calopsita();
+
+    calopsita01.setCor("Amarela");
+    calopsita01.setEspecie("Cockatiel");
+    calopsita01.setTamanho("35cm");
+  }
+}
